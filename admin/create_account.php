bfa8331fec664eaf6b1b0747d112871215d94a18
Dhruvraj Rawat <rawat.dhruv14@gmail.com>
2016-08-12 12:34:18 +0530

    Connected to the Backednd

--- admin/create_account.php
+++ admin/create_account.php
@@ -1,8 +1,53 @@
 <?php session_start();
-if ((!$_SESSION['login_admin'] ) || ($_SESSION['login_privilage'] != 1) ){
-
+include("../includes/config.php");
+if ((!$_SESSION['login_admin'] ) || ($_SESSION['login_privilage'] != 2) ){
     header("Location: ../login.php");
     die();
+}
+
+else{
+
+	if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+	$name=$conn->real_escape_string($_POST['name']);
+    $position=$conn->real_escape_string($_POST['position']);
+    $password=md5($conn->real_escape_string($_POST['password']));
+    $email=$conn->real_escape_string($_POST['email']);
+    $AccountType=$conn->real_escape_string($_POST['AccountType']);
+
+    $currentpassword=md5($conn->real_escape_string($_POST['currentpassword']));
+
+      if ($currentpassword==$_SESSION['login_password']){
+
+ 			if($conn->query("INSERT INTO `users_profile` (`id`, `user_name`,`password`, `email`, `position`, `privilage`) VALUES (NULL, '$name', '$password','$email', '$position', '$AccountType')")){
+
+ 				echo 'The account has been created';
+
+
+
+ 			}
+ 			else{
+
+ 				echo 'The account has not been created. Some Error has occured in DB';
+
+
+ 			}
+
+
+      }
+      else{
+
+      	echo 'The password Entered is incorrect';
+
+
+      }
+
+		
+
+
+
+	}
+
 }
 
 
@@ -11,48 +56,29 @@ if ((!$_SESSION['login_admin'] ) || ($_SESSION['login_privilage'] != 1) ){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Account Creation Wizard">
-	<meta name="author" content="Dhruvraj Singh Rawat">
-	<title>Account Creation Wizard</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Lecture Hall Booking Portal">
+    <meta name="author" content="Dhruvraj Singh Rawat">
+    <title>Dashboard</title>
 
-	<!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../css/basic-template.css" rel="stylesheet" />
 
-	<link href="../css/bootstrap.min.css" rel="stylesheet" />
-	<link href="../css/bootstrap.min.css" rel="stylesheet" />
-
-	<!-- Bootstrap CSS Validator -->
-
-	<link href="../css/bootstrapValidator.min.css" rel="stylesheet" />
-
-	<!-- Jquery and Bootstrap js -->
-
-	<script src="../js/jquery.min.js" text="text/javascript"></script>
-	<script src="../js/bootstrap.min.js" text="text/javascript"></script>
-
-	<!-- Bootstrap Validator js -->
-
-	<script src="../js/bootstrapValidator.min.js" text="text/javascript"></script>
-
-
-
-
+	<!-- BootstrapValidator CSS -->
+    <link href="../css/bootstrapValidator.min.css" rel="stylesheet"/>
+	
+    <!-- jQuery and Bootstrap JS -->
+	<script src="../js/jquery.min.js" type="text/javascript"></script>
+	<script src="../js/bootstrap.min.js" type="text/javascript"></script>
+		
+	<!-- BootstrapValidator -->
+    <script src="../js/bootstrapValidator.min.js" type="text/javascript"></script>
 
 </head>
-
-
-
-
-
-
-
-
-  </head>
-
-  <body>
-
+<body>
   <nav class="navbar navbar-default">
         <div class="container-fluid">
 
@@ -67,8 +93,11 @@ if ((!$_SESSION['login_admin'] ) || ($_SESSION['login_privilage'] != 1) ){
                     <li><a href="index.php">Home</a></li>
                     <li><a href="book.php">Book</a></li>
                     <li><a href="history.php">History</a></li>
-                  
-                    <li   class="active"><a href="create_account.php">Create Account</a></li>
+                    
+                    <li class="active"><a href="create_account.php">Create Account</a></li>
+
+                    
+                    
                    
                 </ul>
             </div>
@@ -83,119 +112,194 @@ if ((!$_SESSION['login_admin'] ) || ($_SESSION['login_privilage'] != 1) ){
         </div>
     </nav>
 
-    <div class="container">
-    	<div class="panel panel-default">
-    		<div class="panel-heading text-center" >Account Creation Wizard</div>
+    <br>
 
-    			<div class="panel-body">
-    				<form id="registration-form" method="POST" class="form-horizontal" action="#">
+	<div class="container">
+		<div class="panel panel-default">
+			<div class="panel-heading text-center">Account Registration</div>
+			<br>
+			<div class="panel-body">
 
+				<form id="registration-form" method="POST" class="form-horizontal" action="#">
 
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="name">Name</label>
+						<div class="col-md-4">
+							<input type="text" class="form-control" id="name" name="name" placeholder="Account Name" />
+						</div>
+					</div>
 
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="position">Position</label>
+						<div class="col-md-4">
+							<input type="text" class="form-control" id="position" name="position" placeholder="User Position" />
+						</div>
+					</div>
 
-    					<div class="form-group">
-    						<label class="col-md-2 control-label" for="email">Email Address</label>
-    						<div class="col-md-4">
-    							<input type="text" class="form-control" id="email" name="email" placeholder="Email Address" />
-    						</div>
-    					</div>
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="email">Email Address</label>
+						<div class="col-md-4">
+							<input type="text" class="form-control" id="email" name="email" placeholder="Email address" />
+						</div>
+					</div>
 
-    					<div class="form-group">
-    						<label class="col-md-2 control-label" for="password">Password</label>
-    						<div class="col-md-4">
-    							<input type="password" class="form-control" id="password" name="password" placeholder="Password" />
-    						</div>
-    					</div>
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="password">Password</label>
+						<div class="col-md-4">
+							<input type="password" class="form-control" id="password" name="password" placeholder="Password" />
+						</div>
+					</div>
 
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="confirmpassword">Confirm Password</label>
+						<div class="col-md-4">
+							<input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirm password" />	
+						</div>
+					</div>	
 
-    					<div class="form-group">
-    						<label class="col-md-2 control-label" for="passwordConfirm">Confirm Password</label>
-    						<div class="col-md-4">
-    							<input type="password" class="form-control" id="passwordConfirm" name="passwordConfirm" placeholder="Confirm Password" />
-    						</div>
-    					</div>
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="AccountType">Account Type</label>
+						<div class="col-md-4">
+							<select class="form-control" id="AccountType" name="AccountType">
+								<option value="0">Choose One</option>
+								<option value="1">Basic Account</option>
+								<option value="2">Admin Account</option>
+							</select>
+						</div>
+					</div>
 
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="confirmpassword">Current Password</label>
+						<div class="col-md-4">
+							<input type="password" class="form-control" id="currentpassword" name="currentpassword" placeholder="Current password" />	
+						</div>
+					</div>
 
-    					<div class="form-group">
-    						<label class="col-md-2 control-label" for="accountType">Account Type</label>
-    						<div class="col-md-4">
-    							<select class="form-control" id="accountType" name="accountType">
-    								<option value="0">Choose One</option>
-    								<option value="1">Admin Account</option>
-    								<option value="2">Basic Account</option>
-    							</select>
-    							
-    						</div>
-    					</div>
+					<div class="form-group">
+						<div class="col-md-6 col-md-offset-2">
+							<button type="submit" class="btn btn-success">Submit</button>
+						</div>
+					</div>
 
-    					<div class="form-group">
-    						<div class="col-md-6 col-md-offset-2">
-    							<buttion type="submit" class="btn btn-success">Submit</buttion>
-    						</div>    						
-    					</div>
+				</form>
 
-    				</form>
+				<div id="confirmation" class="alert alert-success hidden">
+					<span class="glyphicon glyphicon-star"></span> Thank you for registering
+				</div>
 
-    			</div>
-
-    		
-    	</div>
-
-    </div>
-
- 
-
-
-
-     
-
-       
-    	   
-     
-
-
-    <!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-   
-
+			</div>
+		</div>
+	</div>
 </body>
 
 <script type="text/javascript">
-	
 	$(document).ready(function () {
-
 		var validator = $("#registration-form").bootstrapValidator({
-
+			feedbackIcons: {
+				valid: "glyphicon glyphicon-ok",
+				invalid: "glyphicon glyphicon-remove", 
+				validating: "glyphicon glyphicon-refresh"
+			}, 
 			fields : {
-
-				email : {
-
-					message="Email is required"
-
-					validator :{
+				name : {
+					validators: {
 						notEmpty : {
-							message: "Please provide an email Address"
+							message : "Name is required"
+						},
+						stringLength: {
+							min : 5, 
+							max: 35,
+							message: "Name must be between 6 and 35 characters long"
+						},
+ 
+						different : {
+							field : "email", 
+							message: "Email address and name can not match"
 						}
 					}
+				}, 
 
+				position : {
+					validators: {
+						notEmpty : {
+							message : "Position is required"
+						},
+						stringLength: {
+							min : 4, 
+							max: 35,
+							message: "Name must be between 4 and 35 characters long"
+						},
+ 
+
+					}
+				},
+
+
+				email :{
+					message : "Email address is required",
+					validators : {
+						notEmpty : {
+							message : "Please provide an email address"
+						}, 
+						stringLength: {
+							min : 6, 
+							max: 35,
+							message: "Email address must be between 6 and 35 characters long"
+						},
+						emailAddress: {
+							message: "Email address was invalid"
+						}
+					}
+				}, 
+				password : {
+					validators: {
+						notEmpty : {
+							message : "Password is required"
+						},
+						stringLength : {
+							min: 8,
+							message: "Password must be 8 characters long"
+						}, 
+						different : {
+							field : "email", 
+							message: "Email address and password can not match"
+						}
+					}
+				}, 
+				confirmpassword : {
+					validators: {
+						notEmpty : {
+							message: "Confirm password field is required"
+						}, 
+						identical : {
+							field: "password", 
+							message : "Password and confirmation must match"
+						}
+					}
+				}, 
+
+				currentpassword : {
+					validators: {
+						notEmpty : {
+							message: "Current password field is required"
+						}
+						
+					}
+				},
+
+				AccountType : {
+					validators : {
+						greaterThan : {
+							value: 1,
+							message: "Membership is required"
+						}
+					}
 				}
-
 			}
-
-
 		});
+	
 
-
+		
 	});
-
-
 </script>
-
-
-
-
 </html>
